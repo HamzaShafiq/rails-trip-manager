18684a54031c08bc48e43f24c9cf3d9541ef86f8
Hamza Shafiq <hamzashafiq44@gmail.com>
2018-02-18 00:44:27 +0500

    User: Generated scaffold for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217193813) do
+ActiveRecord::Schema.define(version: 20180217194354) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20180217193813) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.string "distance_travelled"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
