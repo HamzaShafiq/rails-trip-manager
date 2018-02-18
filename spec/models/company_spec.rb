9ecdab0880f3357ab04592b4ab1ca5567c12732e
Hamza Shafiq <hamzashafiq44@gmail.com>
2018-02-19 00:17:23 +0500

    Company: Added unit test

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name" do
+    company = Company.new(name: "Test")
+    expect(company).to be_valid
+  end
+  it "is invalid without a name" do
+    company = Company.new(name: nil)
+    company.valid?
+    expect(company.errors[:name]).to include("can't be blank")
+  end
 end
