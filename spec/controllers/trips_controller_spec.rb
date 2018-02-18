4ca9534a8e0b801627e9c9c26ace326778f6d2f6
Hamza Shafiq <hamzashafiq44@gmail.com>
2018-02-19 01:29:54 +0500

    Trip: Removed additional tests code

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -83,47 +83,4 @@ RSpec.describe TripsController, type: :controller do
       end
     end
   end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested trip" do
-        trip = Trip.create! valid_attributes
-        put :update, params: {id: trip.to_param, trip: new_attributes}, session: valid_session
-        trip.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "renders a JSON response with the trip" do
-        trip = Trip.create! valid_attributes
-
-        put :update, params: {id: trip.to_param, trip: valid_attributes}, session: valid_session
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-
-    context "with invalid params" do
-      it "renders a JSON response with errors for the trip" do
-        trip = Trip.create! valid_attributes
-
-        put :update, params: {id: trip.to_param, trip: invalid_attributes}, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq('application/json')
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested trip" do
-      trip = Trip.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: trip.to_param}, session: valid_session
-      }.to change(Trip, :count).by(-1)
-    end
-  end
-
 end
