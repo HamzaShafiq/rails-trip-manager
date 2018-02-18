4805300d8fd16bf9cf2ce33e68cef55302dd2527
Hamza Shafiq <hamzashafiq44@gmail.com>
2018-02-18 16:30:40 +0500

    Added authentication mechanism for authenticating users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
 	belongs_to :company
 	has_many :trips, dependent: :destroy
+
+  before_create :set_api_key
+
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      break token unless User.exists?(api_key: token)
+    end
+  end
+
+  private
+
+    def set_api_key
+      self.api_key = self.generate_api_key
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218092546) do
+ActiveRecord::Schema.define(version: 20180218105127) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180218092546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "company_id"
+    t.string "api_key"
     t.index ["company_id"], name: "index_users_on_company_id"
   end
 
